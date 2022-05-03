a378229f67b7b9116d51c644b49928ee72a38b32
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2022-05-04 00:00:06 +0300

    use transaction types

--- src/Transaction.ts
+++ src/Transaction.ts
@@ -1,23 +1,28 @@
 import { Service } from "./Service";
-import { TransactionRequest } from "./Types";
+import {
+  TransactionDetails,
+  TransactionRequest,
+  TransactionResponse,
+  TransactionStatus,
+} from "./Types";
 
 class TransactionService extends Service {
-  async get(transactionId: string) {
-    const { data } = await this.client.get(
+  async get(transactionId: string): Promise<TransactionDetails> {
+    const { data } = await this.client.get<TransactionDetails>(
       `/mvola/mm/transactions/type/merchantpay/1.0.0/${transactionId}`
     );
     return data;
   }
 
-  async getStatus(serverCorrelationId: string) {
-    const { data } = await this.client.get(
+  async getStatus(serverCorrelationId: string): Promise<TransactionStatus> {
+    const { data } = await this.client.get<TransactionStatus>(
       `/mvola/mm/transactions/type/merchantpay/1.0.0/status/${serverCorrelationId}`
     );
     return data;
   }
 
-  async sendPayment(params: TransactionRequest) {
-    const { data } = await this.client.post(
+  async sendPayment(params: TransactionRequest): Promise<TransactionResponse> {
+    const { data } = await this.client.post<TransactionResponse>(
       "/mvola/mm/transactions/type/merchantpay/1.0.0/",
       { ...params, amount: params.amount.toString() }
     );
