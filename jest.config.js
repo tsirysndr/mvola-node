b93c7bd31aa448517be0fbd49c22fb467ec0dbbb
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2022-08-01 09:51:51 +0300

    add babel-jest and @babel/preset-env

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  testPathIgnorePatterns: ["src", "dist"],
+  testPathIgnorePatterns: [
+    "src",
+    "dist",
+    "node_modules",
+  ],
+ "transformIgnorePatterns": ["/node_modules/(?!@vespaiach/axios-fetch-adapter)"],
+  transform: {
+    '^.+\\.(ts|tsx)?$': 'ts-jest',
+    "^.+\\.(js|jsx)$": "babel-jest",
+  }
 };
